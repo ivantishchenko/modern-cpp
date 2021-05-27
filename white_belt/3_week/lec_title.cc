61028aa0a8d80a8cce917e554f9e654cecf55f6f
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-05-27 23:04:11 +0200

    Solved LectureTitle week3

---
+++ white_belt/3_week/lec_title.cc
@@ -0,0 +1,30 @@
+#include <string>
+
+using namespace std;
+
+struct Specialization {
+  string specialization;
+  explicit Specialization(const string& s) : specialization(s) {}
+};
+
+struct Course {
+  string course;
+  explicit Course(const string& c) : course(c) {}
+};
+
+struct Week {
+  string week;
+  explicit Week(const string& w) : week(w) {}
+};
+
+struct LectureTitle {
+  string specialization;
+  string course;
+  string week;
+
+  LectureTitle(Specialization s, Course c, Week w) : specialization(s.specialization), course(c.course), week(w.week) {} 
+};
+
+int main() {
+  
+}
