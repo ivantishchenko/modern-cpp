34b8f02dbaeb13261a1bf595121c105fc2872e00
Ivan Tishchenko <johnny.tishchenko@gmail.com>
2021-05-29 19:54:28 +0200

    Solved Invert functions

---
+++ white_belt/3_week/inverse_function2.cc
@@ -0,0 +1,62 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class FunctionPart {
+public:
+  FunctionPart(char new_op, double new_val) : op(new_op), val(new_val) {};
+  double Apply(double source) const {
+    if(op == '+') {
+      return source + val;
+    }
+    else if(op == '-') {
+      return source - val;
+    }
+    else if(op == '*') {
+      return source * val;
+    }
+    else {
+      return source / val;
+    }
+  }
+  void Invert() {
+    if(op == '+') {
+      op = '-';
+    }
+    else if(op == '-') {
+      op = '+';
+    }
+    else if(op == '*') {
+      op = '/';
+    }
+    else {
+      op = '*';
+    }
+  }
+private:
+  char op;
+  double val;
+};
+
+class Function {
+public:
+  void AddPart(const char op, double val) {
+    parts.push_back({op, val});
+  }
+  double Apply(double val) const {
+    for(const FunctionPart& part : parts) {
+      val = part.Apply(val);
+    }
+    return val;
+  }
+  void Invert() {
+    for(FunctionPart& part : parts) {
+      part.Invert();
+    }
+    reverse(begin(parts), end(parts));
+  }
+private:
+  vector<FunctionPart> parts;
+};
